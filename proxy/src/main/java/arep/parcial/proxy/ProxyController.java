6ec28bf9bed63b4192c53d529fb5ef1e056a0ac1
unknown <laura.rsanchez@labinfo.is.escuelaing.edu.co>
2025-10-22 11:00:36 -0500

    Proxy con url instancias e instancias activa y pasiva

--- proxy/src/main/java/arep/parcial/proxy/ProxyController.java
+++ proxy/src/main/java/arep/parcial/proxy/ProxyController.java
@@ -15,25 +15,45 @@ import java.util.List;
 public class ProxyController {
 
     int machine = 1;
+    int newValue;
     private static final String USER_AGENT = "Mozilla/5.0";
-
     @GetMapping("/lucasSequence")
     public String lucasSequence(@RequestParam(value = "value") Integer value) throws IOException {
-        if (machine==1){
-            machine = 2;
-            return httpConnection("http://localhost:8080/lucasSequence?value=" + value);
-        }else{
-            machine = 1;
-            return httpConnection("http://localhost:8080/lucasSequence?value=" + value);
-        }
+        newValue = value;
+        return selectMachine();
     }
 
+    private String selectMachine() throws IOException {
+        String URL1 = "http://ec2-52-91-250-75.compute-1.amazonaws.com:8080/lucasSequence?value=" + newValue;
+        String URL2 = "http://ec2-54-227-50-236.compute-1.amazonaws.com:8080/lucasSequence?value=" + newValue;
+        if (machine==1){
+            System.out.println("Machine: " + machine + " with URL: " + URL1);
+            return httpConnection(URL1);
+        }else{
+            System.out.println("Machine: " + machine + " with URL: " + URL2);
+            machine = 1;
+            return httpConnection(URL2);
+        }
+
+    }
     private String httpConnection(String GET_URL) throws IOException {
 
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
+        try{
+            int responseCode = con.getResponseCode();
+        }catch (Exception e){
+            System.out.println("GET request not worked in machine: " + GET_URL );
+            if (machine==1){
+                machine = 2;
+            }else{
+                machine = 1;
+            }
+            System.out.println("Machine: " + machine);
+            return selectMachine();
+        }
 
         //The following invocation perform the connection implicitly before getting the code
         int responseCode = con.getResponseCode();
@@ -54,8 +74,14 @@ public class ProxyController {
             System.out.println(response.toString());
             return response.toString();
         } else {
-            System.out.println("GET request not worked");
-            return "";
+            System.out.println("GET request not worked in machine: " + GET_URL );
+            if (machine==1){
+                machine = 2;
+            }else{
+                machine = 1;
+            }
+            System.out.println("Change Machine to: " + machine);
+            return selectMachine();
         }
     }
 
